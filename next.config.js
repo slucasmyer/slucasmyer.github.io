698c78c3fa876b0d19553f5205de1a238b74d584
Sullivan Myer <slucasmyer@icloud.com>
2022-11-22 11:54:43 -0500

    Sullivan Myer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  webpack(config) {
+    config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm'
+    //config.experiments = { ...config.experiments, asyncWebAssembly: true }
+    return config
+  },
 }
 
